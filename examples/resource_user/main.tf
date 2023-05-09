79b8557f3858e88f7148023a70a104147e43d4a7
shreyas3-vmw <132661158+shreyas3-vmw@users.noreply.github.com>
2023-05-09 18:16:53 +0530

    Terraform Provider for Service Accounts - Review Comments Changes

--- examples/resource_user/main.tf
+++ examples/resource_user/main.tf
@@ -20,21 +20,21 @@ data "vmds_roles" "all" {
 }
 
 output "roles_data" {
-  value = data.mds_roles.all
+  value = data.vmds_roles.all
 }
 
 data "vmds_policies" "policies" {
 }
 
 output "policies_data" {
-  value = data.mds_policies.policies
+  value = data.vmds_policies.policies
 }
 
 resource "vmds_user" "temp" {
   email      = "developer@vmware.com"
   policy_ids = ["64539a8f7d85190f7e5ae1e1"]
   tags       = ["new-user-tf", "update-tf-user"]
-  role_ids   = [for role in data.mds_roles.all.roles : role.role_id if contains(local.service_roles, role.name)]
+  role_ids   = [for role in data.vmds_roles.all.roles : role.role_id if contains(local.service_roles, role.name)]
   timeouts   = {
     create = "1m"
   }
